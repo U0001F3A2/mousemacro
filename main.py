2176df67bb59507ff30e4bfe9db540056eac1e03
notreal coder <js951204@naver.com>
2018-08-19 12:50:09 -0400

    implemented mouse click point, move duration, duration between clicks, iteration wait time randomization. (first working version)

--- main.py
+++ main.py
@@ -1,13 +1,78 @@
-import pyautogui, win32gui
+import pyautogui, win32gui, pyhooked, time, threading, datetime, sys, random
 
-cursorpos = win32gui.GetCursorPos()
-print(cursorpos)
-pyautogui.mouseDown(None,None,'left')
-pyautogui.mouseUp(None,None,'left')
+#
+# cursorpos = win32gui.GetCursorPos()
+# print(cursorpos)
+# pyautogui.mouseDown(None, None, 'left')
+# pyautogui.mouseUp(None, None, 'left')
+#
+# print(cursorpos[0])
+# print(cursorpos[1])
+# print(cursorpos[1:])
+start_time = datetime.datetime.now()
+activity_tracker = []
 
-print(cursorpos[0])
-print(cursorpos[1])
-print(cursorpos[1:])
+def handle_event(args):
+    current_time = datetime.datetime.now()
+    delta = current_time - start_time
+    time = delta.seconds + delta.microseconds/1000000
 
-def main():
-    pass
+    if isinstance(args, pyhooked.KeyboardEvent):
+        print(args.key_code)
+        if args.current_key == 'A' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
+            print("Ctrl + A was pressed")
+        elif args.current_key == 'Q' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
+            myhook.unhook_mouse()
+            myhook.unhook_keyboard()
+            myhook.stop()
+            print('Quitting.')
+
+
+    if isinstance(args, pyhooked.MouseEvent):
+        if args.event_type == "key down":
+            print("("+ str(args.mouse_x) + ", " + str(args.mouse_y) + ")" + "  " + str(time))
+            if len(activity_tracker)==0:
+                activity_tracker.append([(args.mouse_x, args.mouse_y), time])
+            else:
+                activity_tracker.append([(args.mouse_x, args.mouse_y), time-activity_tracker[-1][1]])
+
+
+def wait():
+    time.sleep(10)
+    myhook.unhook_mouse()
+    return
+
+# mythread = threading.Thread(target=wait)
+# mythread.daemon=True
+
+print(1)
+myhook = pyhooked.Hook()
+myhook.handler = handle_event
+print(1)
+myhook.hook(keyboard=True, mouse=True)
+# activity_tracker = activity_tracker[:len(activity_tracker)-2]
+print(1)
+print(activity_tracker)
+# mythread.start()
+
+def rand_mouse_coord(coordinate):
+    x_original = coordinate[0]
+    y_original = coordinate[1]
+    x = random.randint(x_original - 10, x_original + 10)
+    y = random.randint(y_original - 10, y_original + 10)
+    return (x,y)
+
+def rand_time(time):
+    mult = random.randint(0,100)
+    return time * ( 1 + mult/100)
+
+while True:
+    new_activity_list = []
+    for act in activity_tracker:
+        new_activity_list.append([rand_mouse_coord(act[0]), rand_time(act[1])])
+    for activity in new_activity_list:
+        move_duration = random.randint(1,8) / 10
+        time.sleep(activity[1])
+        pyautogui.moveTo(activity[0][0],activity[0][1],move_duration)
+        pyautogui.click(None,None)
+    time.sleep(random.randint(1,120))
